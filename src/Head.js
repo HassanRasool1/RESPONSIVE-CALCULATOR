4a8afbb5f41476b313a9982260ef5215a636418e
Hassan Rasool <109318661+HassanRasool1@users.noreply.github.com>
2023-12-01 11:24:53 +0500

    Add files via upload

---
+++ src/Head.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Head.css'
+
+const Head = () => {
+  return (
+    <div>
+        <div className='heading'>
+        <p>Active calculator.........</p>
+      </div>
+    </div>
+  )
+}
+
+export default Head
